16e2464c5972736dd9bbaf1f296557c94b1b8c15
ahmedanwer <ahmedmuhanwar@gmail.com>
2015-12-17 19:58:16 +0200

    detecting only new people but i have a problem in removing false tracks

--- tracking.py
+++ tracking.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import imutils
-import random
+from random import randint
 from imutils.object_detection import non_max_suppression
 
 cv2.namedWindow("tracking")
@@ -9,38 +9,48 @@ camera = cv2.VideoCapture("t.avi")
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-tracker = cv2.MultiTracker("TLD")
+tracker = cv2.MultiTracker("KCF")
 
 FirstType=False
-color1=(250,0,0)
-color2=(0,250,0)
-color3=(0,0,250)
-boxes=  np.array([0])
+colors = [(255,0,0),(0,255,0),(0,0,255),
+        (255,255,0),(0,255,255),(255,0,255),(0,0,0),(255,255,255),(0,0,0),(120,120,120)]
+
+
+boxes=  np.array([[0,0,0,0]])
 j=0
 while camera.isOpened():
     ok, image=camera.read()
-    #image = imutils.resize(image, width=min(400, image.shape[1]))
+    #image = imutils.resize(image, width=min(600, image.shape[1]))
     if FirstType==False:
         # detect people in the image
-        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),padding=(8, 8), scale=0.5)
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4,4), padding=(8,8), scale=0.3)
         # draw the original bounding boxes
         for (x, y, w, h) in rects:
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
             bsbox=(x,y,w,h)
-            tracker.add(image,bsbox)
+            for newbox in boxes:
+                px = int(newbox[0])-x
+                py = int(newbox[1])-y
+                if(abs(px)>70 and abs(py)>70):
+                    tracker.add(image,bsbox)
         FirstType=True
     j=j+1
-    if j==15:
+    if j==20:
         FirstType=False
         print("Reset")
-        j=0;
+        j=0
     ok, boxes = tracker.update(image)
     #print(boxes)
+    COrder=0
     for newbox in boxes:
+        if COrder>9:
+            COrder=0
         p1 = (int(newbox[0]), int(newbox[1]))
         p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
-        cv2.rectangle(image, p1, p2, (250,0,0))
-
+        cv2.rectangle(image, p1, p2, colors[COrder])
+        COrder=COrder+1
+    COrder=0
+    print(boxes)
     cv2.imshow("tracking", image)
     k = cv2.waitKey(1) & 0xff
     if k == 27 : break # esc pressed
